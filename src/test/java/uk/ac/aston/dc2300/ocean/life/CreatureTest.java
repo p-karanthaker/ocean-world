a08397ab6592c029e6c320b5a88912e822471d12
Karan Thaker <karan.thaker@hotmail.co.uk>
2016-04-10 18:56:50 +0100

    Tests added to build path
    Added some dummy tests so that the files are included on the git
    project.

---
+++ src/test/java/uk/ac/aston/dc2300/ocean/life/CreatureTest.java
@@ -0,0 +1,19 @@
+package uk.ac.aston.dc2300.ocean.life;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Dummy test class...
+ * @author Karan Thaker
+ *
+ */
+public class CreatureTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/uk/ac/aston/dc2300/ocean/world/WorldTest.java
@@ -0,0 +1,19 @@
+package uk.ac.aston.dc2300.ocean.world;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Dummy test class...
+ * @author Karan Thaker
+ *
+ */
+public class WorldTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
