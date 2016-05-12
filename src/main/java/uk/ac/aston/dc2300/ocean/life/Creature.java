88eeb4f410f589ff619a430ca3f3601219bea6e9
Karan Thaker <karan.thaker@hotmail.co.uk>
2016-05-12 20:35:39 +0100

    Creature and Plankton created
    Created Creature class and Plankton class...

--- src/main/java/uk/ac/aston/dc2300/ocean/life/Creature.java
+++ src/main/java/uk/ac/aston/dc2300/ocean/life/Creature.java
@@ -1,11 +1,51 @@
 package uk.ac.aston.dc2300.ocean.life;
 
+import uk.ac.aston.dc2300.ocean.world.Location;
+
 /**
- * Creature class created to remove errors.
+ * Creature class.
  * 
  * @author Karan Thaker
  *
  */
 public class Creature {
-
+	
+	private String species;
+	private int age;
+	private int nutritionValue;
+	private Location location;
+	
+	public Creature() {
+		age = 0;
+	}
+	
+	public Creature(String species) {
+		this.species = species;
+		age = 0;
+	}
+	
+	public String getSpecies() {
+		return species;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public Location getLocation() {
+		return location;
+	}
+	
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+	
 }
---
+++ src/main/java/uk/ac/aston/dc2300/ocean/life/Plankton.java
@@ -0,0 +1,9 @@
+package uk.ac.aston.dc2300.ocean.life;
+
+public class Plankton extends Creature {
+	
+	public Plankton() {
+		
+	}
+
+}
