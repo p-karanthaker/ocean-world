9037c3f0ac0172c118f738005d69f4e1d60cef4d
Karan Thaker <p-karanthaker@users.noreply.github.com>
2016-05-17 21:38:40 +0100

    Created ModelConstants (#5)

---
+++ src/main/java/uk/ac/aston/dc2300/ocean/world/ModelConstants.java
@@ -0,0 +1,10 @@
+package uk.ac.aston.dc2300.ocean.world;
+
+public class ModelConstants {
+
+	public static int SIMULATION_LENGTH = 1000;
+	public static int RNG_SEED = 44;
+	public static int OCEAN_WIDTH = 100;
+	public static int OCEAN_DEPTH = 100;
+	
+}
