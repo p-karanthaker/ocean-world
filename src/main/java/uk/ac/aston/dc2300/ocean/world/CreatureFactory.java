8d0657d81dcef0b3e2e7695a72319fc2739f2593
Karan Thaker <p-karanthaker@users.noreply.github.com>
2016-05-18 12:30:01 +0100

    Updated classes to sync with creature changes. (#8)

--- src/main/java/uk/ac/aston/dc2300/ocean/world/CreatureFactory.java
+++ src/main/java/uk/ac/aston/dc2300/ocean/world/CreatureFactory.java
@@ -12,41 +12,16 @@ import uk.ac.aston.dc2300.ocean.life.*;
  */
 public class CreatureFactory {
     
-    public Creature getCreature(String species){
-        if(species == null){
-            return null;
-        }
-        else if(species == "Sardine"){
-            return new Sardine(75, 12, 14, 0.1f, 0.5f);
-        }
-        else if(species == "Shark"){
-            return new Shark(150, 25, 0, 0.05f, 0.2f);
-        }
-        else if(species == "Plankton"){
-            return new Plankton(100, 0, 4, 0.7f, 0.8f);
-        }
-        
-        return null;
-    }
-    
-    public Creature getCreature(String species, int maxAge, int minBreedingAge, 
-            int nutritionalValue, float creationOdds, float breedingOdds){
-        if(species == null){
-            return null;
-        }
-        else if(species == "Sardine"){
-            return new Sardine(maxAge, minBreedingAge, nutritionalValue, 
-                    creationOdds, breedingOdds);
-        }
-        else if(species == "Shark"){
-            return new Shark(maxAge, minBreedingAge, nutritionalValue, 
-                    creationOdds, breedingOdds);
-        }
-        else if(species == "Plankton"){
-            return new Plankton(maxAge, minBreedingAge, nutritionalValue, 
-                    creationOdds, breedingOdds);
-        }
-        
-        return null;
+    public static Creature getCreature(Species species){
+    	switch (species) {
+	    	case PLANKTON:
+	    		return new Plankton();
+	    	case SARDINE:
+	    		return new Sardine();
+	    	case SHARK:
+	    		return new Shark();
+	    	default:
+	    		return null;
+    	}
     }
 }
