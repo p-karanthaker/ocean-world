d6ad48edc892a1ca7aa02ad80acb883c61cb78ad
Karan Thaker <karan.thaker@hotmail.co.uk>
2016-05-21 18:50:58 +0100

    Proposed changed to act/

--- src/main/java/uk/ac/aston/dc2300/ocean/life/Shark.java
+++ src/main/java/uk/ac/aston/dc2300/ocean/life/Shark.java
@@ -11,7 +11,12 @@ public class Shark extends Fish {
 
 	@Override
 	public void act(Field field) {
-		super.act(field);
+		if(findFood(field).isEmpty()) {
+			// move somewhere using super?
+			super.act(field);
+		} else {
+			// try to eat
+		}
 	}
 	
 }
